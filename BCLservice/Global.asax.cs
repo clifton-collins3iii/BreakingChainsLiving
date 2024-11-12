b499cdcfbe736da996c612cf0dde5fea1ddea8dc
cliftontrendsic <clifton@trendsic.com>
2024-11-11 19:47:50 -0600

    having trouble with license and ss v8.40

--- BCLservice/Global.asax.cs
+++ BCLservice/Global.asax.cs
@@ -6,6 +6,7 @@ namespace BCLservice
     {
         protected void Application_Start(object sender, EventArgs e)
         {
+//            ServiceStack.Licensing.RegisterLicense(@"wvpkPXGeEvOJ16y2Vd/cOFxgjNXghGwzOrVLJ43Ajp5qvWMxZ1mS0MmWgq4MJStsuGstvsWnj4ZaKfZ0tRtEfa4a15STHsZDAa7Qv950b+nMJGzA7vm/dzrnnctl/v5sHZd9MX7iCrHDW03hPC7/zDGj/I3oOtCyVDQelMCOxvI=");
             new AppHost().Init();
         }
     }
